0811c72bacce6d8adc7a25be8292ba55f4118d9e
Alex Crichton <alex@alexcrichton.com>
2015-02-03 12:37:01 -0800

    Bump to 0.1.12

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,7 @@
 
 #![feature(slicing_syntax, collections, core, hash, rand, std_misc)]
 #![cfg_attr(test, deny(warnings))]
+#![cfg_attr(test, feature(test))]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/num/",
