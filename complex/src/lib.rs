c7c974ec4bc2d625ae50488c626e72377bfd8929
Adam Crume <adamcrume@gmail.com>
2016-06-28 19:44:52 -0700

    Implement Default for Complex
    Fixes #198

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -29,7 +29,7 @@ use traits::{Zero, One, Num, Float};
 // probably doesn't map to C's _Complex correctly.
 
 /// A complex number in Cartesian form.
-#[derive(PartialEq, Copy, Clone, Hash, Debug)]
+#[derive(PartialEq, Copy, Clone, Hash, Debug, Default)]
 #[cfg_attr(feature = "rustc-serialize", derive(RustcEncodable, RustcDecodable))]
 pub struct Complex<T> {
     /// Real portion of the complex number
