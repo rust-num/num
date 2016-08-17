4f6f7b329277993092f2981346fb7defb39d4406
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2016-08-17 23:12:20 +0200

    Add travis build test of 0.7.0 channel

--- .travis/test_nightly.sh
+++ .travis/test_nightly.sh
@@ -8,3 +8,7 @@ cargo test --verbose --manifest-path=macros/Cargo.toml
 
 # Build test for the serde feature
 cargo build --verbose --features "serde"
+
+# Downgrade serde and build test the 0.7.0 channel as well
+cargo update -p serde --precise 0.7.0
+cargo build --verbose --features "serde"
