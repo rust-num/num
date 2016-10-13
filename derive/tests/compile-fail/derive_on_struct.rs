97551ade5ba6872b436b6b4e117007c47181503f
Josh Stone <cuviper@gmail.com>
2016-10-13 10:31:49 -0700

    derive: update to proc-macro

--- derive/tests/compile-fail/derive_on_struct.rs
+++ derive/tests/compile-fail/derive_on_struct.rs
@@ -9,7 +9,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(rustc_macro)]
+#![feature(proc_macro)]
 
 extern crate num;
 #[macro_use]
--- derive/tests/with_custom_values.rs
+++ derive/tests/with_custom_values.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(rustc_macro)]
+#![feature(proc_macro)]
 
 extern crate num;
 #[macro_use]
