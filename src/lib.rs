07ff5b62b92d500a9f13ad0de66b414f341accc7
Jacob Kiesel <kieseljake@gmail.com>
2017-02-07 09:52:23 -0700

    Changing to >= and <= as it's a slight optimization

--- src/lib.rs
+++ src/lib.rs
@@ -112,7 +112,7 @@ pub mod rational {
 
 pub fn clamp<T: PartialOrd + Copy>(input: T, min: T, max: T) -> T {
     debug_assert!(min < max, "min must be less than max");
-    if input < min {min}
-    else if input > max {max}
+    if input <= min {min}
+    else if input >= max {max}
     else {input}
 }
