81aa6dbe486d06b58e136bf3e15904e360072c2b
Yoan Lecoq <yoanlecoq.io@gmail.com>
2017-04-30 08:29:21 +0200

    Revert impl for Checked and Saturating

--- traits/src/ops/saturating.rs
+++ traits/src/ops/saturating.rs
@@ -1,5 +1,3 @@
-use std::num::Wrapping;
-
 /// Saturating math operations
 pub trait Saturating {
     /// Saturating addition operator.
@@ -28,10 +26,3 @@ macro_rules! saturating_impl {
 }
 
 saturating_impl!(Saturating for isize usize i8 u8 i16 u16 i32 u32 i64 u64);
-
-impl<T: Saturating> Saturating for Wrapping<T> {
-    #[inline]
-    fn saturating_add(self, v: Self) -> Self { Wrapping(self.0.saturating_add(v.0)) }
-    #[inline]
-    fn saturating_sub(self, v: Self) -> Self { Wrapping(self.0.saturating_sub(v.0)) }
-}
