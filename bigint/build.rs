d4e9bb41436e2acbdfdc2d03ffe14ccb8a03c441
hcpl <hcpl.prog@gmail.com>
2017-12-13 17:52:13 +0200

    bigint: automatically enforce cfg option usages
    Uses runtime checks when testing (I couldn't think of better solutions
    that would work for Rust 1.8+).

--- bigint/build.rs
+++ bigint/build.rs
@@ -1,10 +1,31 @@
 extern crate rustc_version;
 
-use rustc_version::version_matches;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+use rustc_version::{version, version_matches};
+
+
+fn write_build_info() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("build_info.rs");
+    let mut f = File::create(&dest_path).unwrap();
+
+    write!(&mut f, "
+        pub const RUSTC_VERSION: &'static str = {:?};
+    ", version().to_string()).unwrap();
+}
+
+fn print_cfg_options() {
+    if version_matches(">= 1.12.0") {
+        println!("cargo:rustc-cfg=impl_sum_product_for_bigints");
+    }
+}
 
 
 fn main() {
-    if version_matches(">=1.12.0") {
-        println!("cargo:rustc-cfg=impl_sum_product_for_bigints");
-    }
+    write_build_info();
+    print_cfg_options();
 }
