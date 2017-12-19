7bcceaea8395f2751ef597bd54533cc875ed71ba
Josh Stone <cuviper@gmail.com>
2017-12-18 17:34:34 -0800

    Move all of the num sub-crates to their own repos

--- ci/rustup.sh
+++ ci/rustup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Use rustup to locally run the same suite of tests as .travis.yml.
-# (You should first install/update 1.8.0, 1.15.0, beta, and nightly.)
+# (You should first install/update 1.8.0, stable, beta, and nightly.)
 
 set -ex
 
 export TRAVIS_RUST_VERSION
-for TRAVIS_RUST_VERSION in 1.8.0 1.15.0 beta nightly; do
+for TRAVIS_RUST_VERSION in 1.8.0 stable beta nightly; do
     run="rustup run $TRAVIS_RUST_VERSION"
     if [ "$TRAVIS_RUST_VERSION" = 1.8.0 ]; then
       # libc 0.2.34 started using #[deprecated]
@@ -14,5 +14,4 @@ for TRAVIS_RUST_VERSION in 1.8.0 1.15.0 beta nightly; do
     fi
     $run cargo build --verbose
     $run $PWD/ci/test_full.sh
-    $run cargo doc
 done
